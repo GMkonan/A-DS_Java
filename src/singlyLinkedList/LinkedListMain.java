f1718ffa421ca4d2c5726987b6ac44b2497b6c04
konan <guilhemont@hotmail.com>
2020-12-02 18:19:35 -0300

    made insert and delete methods to singlylinkedlist

--- src/singlyLinkedList/LinkedListMain.java
+++ src/singlyLinkedList/LinkedListMain.java
@@ -1,5 +1,31 @@
 package singlyLinkedList;
 
 public class LinkedListMain {
-
+	public static void main(String args[]) {
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		linkedList.insert(1);
+		linkedList.insert(8);
+		linkedList.insert(5);
+		//its gonna be 5 -> 8 -> 1
+		
+		linkedList.insertLast(3);
+		//now its gonna be 5 -> 8 -> 1 -> 3
+		Node node = new Node();
+		node.data = 1;
+		linkedList.insertAnyWhere(node, 10);
+		// now its gonna be 5 -> 8 -> 1 -> 10 -> 3
+		Node node2 = new Node();
+		node2.data = 8;
+		linkedList.deleteAfter(node2);
+		// now its gonna be 5 -> 8 -> 10 -> 3
+		linkedList.deleteFirst();
+		//now its gonna be 8 -> 10 -> 3
+		linkedList.insert(14);
+		//now its gonna be 14 -> 8 -> 10 -> 3
+		linkedList.insertLast(40);
+		// now its gonna be 14 -> 8 -> 10 -> 3 -> 40
+		linkedList.deleteFirst();
+		//now its gonna be 8 -> 10 -> 3 -> 40
+		linkedList.printLinkedList();
+	}
 }
--- src/singlyLinkedList/SinglyLinkedList.java
+++ src/singlyLinkedList/SinglyLinkedList.java
@@ -17,10 +17,68 @@ class Node {
 }
 
 public class SinglyLinkedList {
-	//This one is the first Node and it can have data or
-	//not, search "dummy headers",is used for condition
+	//This one is the first Node and it can be a dummy or
+	//not, used to not need to specify condition null
 	//Its private because we only need to reference head
 	//inside this class
 	private Node head;
 	
+	public void insert(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void insertLast(int data) {
+		Node current = head; //start from the first node
+		//loop until current == null because you are looping
+		//all the nodes until you find the end (null)
+		while(current.next != null) {
+			current = current.next;
+		}
+		//insert the newNode next to current
+		//which now is the final node
+		Node newNode = new Node();
+		newNode.data = data;
+		current.next = newNode;
+	}
+	
+	public void insertAnyWhere(Node after,int data) {
+		Node current = head;
+		Node temp = head;
+		while (current.data != after.data ) {
+			current = temp;
+			temp = temp.next;
+		}
+		Node newNode = new Node();
+		newNode.data = data;
+		current.next = newNode;
+		newNode.next = temp;
+	}
+	
+	public void deleteAfter(Node after) {
+		Node current = head; 
+		while (current.data != after.data && current.next != null) {
+			current = current.next;
+		}
+		current.next = current.next.next;
+	}
+	
+	public void deleteFirst() {
+		//shifts the first place to be where 
+		//it was pointing (second place)
+		head = head.next;
+	}
+	
+	public void printLinkedList() {
+		Node current = head;
+		//maybe change the way is displayed to nodeData1 -> nodeData2...
+		while (current != null) {
+			current.displayNodeData();
+			current = current.next;
+			
+		}
+
+	}
 }
