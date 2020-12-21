48ad7ef0147a35dca0c9b33b683b4de33c0b3f31
GMkonan <guilhemont@hotmail.com>
2020-12-21 04:39:17 -0300

    finished simple implementation binarysearch

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -1,5 +1,34 @@
 package binarySearch;
 
 public class BinarySearch {
-
+	int search(int[] arr, int key) {
+		
+		//need to start writing the comments explaining
+		
+		//just a placeholder
+		int index = 0;
+		
+		int first = 0;
+		int last = arr.length - 1;
+		int mid = (first + last)/2;
+		
+		while(first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			}
+			else if (arr[mid] == key) {
+				index = mid;
+				break;
+			}
+			else {
+				last = mid - 1;
+			}
+			// make new count now with the new value to first or last
+			mid = (first + last)/2;
+		}
+		if (first > last) {
+			System.out.println("not found 404");
+		}
+		return index;
+	}
 }
--- src/binarySearch/BinarySearchMain.java
+++ src/binarySearch/BinarySearchMain.java
@@ -2,6 +2,12 @@ package binarySearch;
 
 public class BinarySearchMain {
 	public static void main(String args[]) {
+		BinarySearch binary = new BinarySearch();
+		int[] arr = {10, 6, 4, 8, 2};
+		int key = 8;
+		
+		System.out.println("The key is in position " + binary.search(arr, key));
+		
 		
 	}
 }
